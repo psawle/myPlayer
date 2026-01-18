c9280806467c7aa302f98ae98b5a1dcb2c1f52ff
Prashant Sawle <psawle4@gmail.com>
2026-01-18 22:28:00 +0530

    working on moody player

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
- function connectToDB () {
-
-    mongoose.connect("mongodb+srv://psawle4_db_user:UguQnfp4Ez6eGN79@cluster0.cim2kbu.mongodb.net/natours").then(() => {
-        console.log("Connected to db")
+function connectDB () {
+    mongoose.connect(process.env.MONGODB_URL).then(() => {
+        console.log("connected to db")
+    }).catch(() => {
+        console.log("Error")
     })
 }
 
-module.exports = connectToDB
+module.exports = connectDB;
